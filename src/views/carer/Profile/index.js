bdb83bdb3f3807fd2a64350ad4c166bf35a5e8fc
Figo <figolee2127@gmail.com>
2020-11-06 20:28:43 +0800

    new carer page

--- src/views/carer/Profile/index.js
+++ src/views/carer/Profile/index.js
@@ -1,21 +1,82 @@
 import React from 'react'
+import CarerNav from '../../../components/common/CarerNav'
+
 import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
-import CarerNav from '../../../components/common/CarerInfo'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles((theme) => ({
   cardGrid: {
     paddingTop: theme.spacing(10),
     paddingBottom: theme.spacing(3),
   },
+  table: {
+    minWidth: 800,
+  },
 }))
 
-export default function Home() {
-  const classes = useStyles()
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.secondary.main,
+    color: theme.palette.common.black,
+    fontSize: 18,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+const columns = [
+  "carer_name", "rating", "is_fulltime", "phone", "area"
+];
+
+export default function ProfileView({ data }) {
+
+  const classes = useStyles();
+
+  const dataToString = (row, col) => {
+    const data = row[col];
+    if (col.search("date") >= 0) {
+      return data ? new Date(data).toLocaleString() : "";
+    }
+    return data ? String(data) : "";
+  }
+  
   return (
     <div>
-      <Container className={classes.cardGrid} maxWidth="lg">
-      <CarerInfo />
+      <CarerNav />
+      <Container className={classes.cardGrid} maxWidth="md">
+        <TableContainer component={Paper}>
+          <Table className={classes.table} size="small" aria-label="a dense table">
+            <TableHead>
+              <StyledTableRow>
+                {columns.map((col, i) => <StyledTableCell key={i}>{ col }</StyledTableCell>)}
+              </StyledTableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row, i) => (
+                <StyledTableRow key={i}>
+                  { columns.map((col, j) => <StyledTableCell key={`${i}${j}`}>{ dataToString(row, col) }</StyledTableCell>)}
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Container>
     </div>
   )
