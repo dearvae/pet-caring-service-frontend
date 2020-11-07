8d5d33e0cc655442462bdd5265830d3083e8476a
dearvae <lovevae7@gmail.com>
2020-11-07 19:41:35 +0800

    update bid

--- pages/owner/pet/pets.js
+++ pages/owner/pet/pets.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, useCallback } from 'react';
+import AddIcon from '@material-ui/icons/Add';
 import 'antd/dist/antd.css';
-import { Typography } from 'antd';
-
+import { Typography, Modal, Form, Input  } from 'antd';
+import Fab from '@material-ui/core/Fab';
 import PetTable from '../../../src/components/pet/PetTable';
 import OwnerNav from '../../../src/components/common/OwnerNav';
 
@@ -17,13 +17,48 @@ const useStyles = makeStyles((theme) => ({
 	},
 	title: {
 		textAlign: 'center'
-	}
+	},
+	fab: {
+		position: 'absolute',
+		bottom: theme.spacing(10),
+		right: theme.spacing(10),
+	  },
 }));
 
 export default function Home(props) {
 	const classes = useStyles();
 	const [username, setUsername] = useState("");
 	const [petList, setPetList] = useState([]);
+	const [visible, setVisible] = useState(false);
+	const [form] = Form.useForm();
+	const showAddModal = () => {
+        setVisible(true);
+    };
+	const closePopup = useCallback(() => {
+        form.resetFields();
+        setEditVisible(false);
+      }, [form]);
+
+
+      const onSubmit = useCallback((values) => {
+		setVisible(false);
+		console.log(values);
+        const postMethod = {
+            method: 'POST', // Method itself
+            headers: {
+                "Content-Type": "application/json",
+              },
+            body: JSON.stringify({
+				owner_name: localStorage.getItem('username'),
+				pname: values.pname,
+                requirements : values.requirements,
+                belongs: values.belongs
+            })  // We send data in JSON format
+           }
+           console.log( postMethod.body);
+        const url = `${process.env.NEXT_PUBLIC_API_PATH}/pets/`;
+        fetch(url, postMethod).then(window.location.reload());
+        }, []);
 	useEffect(() => {
 		setUsername(localStorage.getItem('username'));
 		fetch(`${process.env.NEXT_PUBLIC_API_PATH}/pets/${username}`)
@@ -34,6 +69,31 @@ export default function Home(props) {
 	  });
 	return (
 		<div className={classes.root}>
+			<Modal
+				title={"Add a new pet"}
+				visible={visible}
+				onOk={form.submit}
+				okText="Add"
+				onCancel={closePopup}
+				>
+					<Form
+						form={form}
+						onFinish={onSubmit}
+					>
+					<Form.Item label="Pet Name" name="pname">
+					<Input />
+					</Form.Item>
+					<Form.Item label="Requirements" name="requirements">
+					<Input />
+					</Form.Item>
+					<Form.Item label="Category" name="belongs">
+					<Input />
+					</Form.Item>
+					</Form>
+				</Modal>
+			<Fab className={classes.fab} color="primary" aria-label="add" onClick={showAddModal}>
+				<AddIcon />
+			</Fab>
 			<OwnerNav />
 			<Title className={classes.title}>Pets List</Title>
 			<PetTable petList={petList} />
